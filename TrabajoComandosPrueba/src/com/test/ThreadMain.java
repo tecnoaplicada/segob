34c89b20652874a7d571cc8b9422311f315b56d7
Anthoni Olvera <Anthoni Olvera@192.168.1.71>
2020-01-16 00:21:25 -0600

    SE ELIMINA LINEA

--- TrabajoComandosPrueba/src/com/test/ThreadMain.java
+++ TrabajoComandosPrueba/src/com/test/ThreadMain.java
@@ -5,7 +5,7 @@ public class ThreadMain {
 	
 	public static void main( String args[]) {
 		System.out.println("Hello Git Hub SE REALIZA CAMBIO EN ARCHIVO");
-		System.out.println("Hello Git Hub SE REALIZA CAMBIO EN ARCHIVO");
+ 
 
 	
 	}
