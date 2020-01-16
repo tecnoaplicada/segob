7fe1f6c02a7765f0c154d7ae4bbc5134dec038ab
Anthoni Olvera <Anthoni Olvera@192.168.1.71>
2020-01-15 23:53:30 -0600

    se realiza cambi de main cadena salida consola

--- TrabajoComandosPrueba/src/com/test/ThreadMain.java
+++ TrabajoComandosPrueba/src/com/test/ThreadMain.java
@@ -4,6 +4,6 @@ public class ThreadMain {
 
 	
 	public static void main( String args[]) {
-		System.out.println("Hello Git Hub");
+		System.out.println("Hello Git Hub SE REALIZA CAMBIO EN ARCHIVO");
 	}
 }
