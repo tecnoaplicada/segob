51a3c6d9fb29739ff181278598a6fbf66ab8e700
Anthoni Olvera <Anthoni Olvera@192.168.1.71>
2020-01-19 21:12:06 -0600

    BNJKLHBHJKBJKBJLKN

--- AplicacionNetBeans/src/aplicacionnetbeans/AplicacionNetBeans.java
+++ AplicacionNetBeans/src/aplicacionnetbeans/AplicacionNetBeans.java
@@ -23,6 +23,7 @@ public class AplicacionNetBeans {
         System.out.println("Este es un cambio en netbeans D");
         System.out.println("Este es un cambio en netbeans E");
         System.out.println("Este es un cambio en netbeans F    ");
+        System.out.println("Este es un cambio en netbeans G    ");
 
     }
     
