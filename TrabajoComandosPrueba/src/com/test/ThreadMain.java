691e6d59dc9215680e0998d9f983ae39fb095297
tony <antonio.olvera@tecnoaplicada.com>
2020-01-21 23:34:41 -0600

    SE REALIZAN AJUSTES DE FECHA

--- TrabajoComandosPrueba/src/com/test/ThreadMain.java
+++ TrabajoComandosPrueba/src/com/test/ThreadMain.java
@@ -16,6 +16,9 @@ public class ThreadMain {
  		System.out.println( locadDate );
  		System.out.println( LocalDate.parse("2000-01-02", DateTimeFormatter.ISO_DATE) );
 
+ 		System.out.println( locadDate );
+ 		System.out.println( LocalDate.parse("2000-01-02", DateTimeFormatter.ISO_DATE) );
+
 	
 	}
 }
