6f1682044243e37dfc30c485accb8d0a9395f2b6
tony <antonio.olvera@tecnoaplicada.com>
2020-01-21 23:22:03 -0600

    SE REALIZA EJERCICIO DE FECHAS JAVA 8

--- TrabajoComandosPrueba/src/com/test/ThreadMain.java
+++ TrabajoComandosPrueba/src/com/test/ThreadMain.java
@@ -1,11 +1,20 @@
 package com.test;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class ThreadMain {
 
 	
 	public static void main( String args[]) {
 		System.out.println("Hello Git Hub SE REALIZA CAMBIO EN ARCHIVO");
  		System.out.println("Segunda linea commit en HIT HUB");
+ 		
+ 		LocalDate locadDate = LocalDate.of(2000, 3, 4);
+ 	
+ 		
+ 		System.out.println( locadDate );
+ 		System.out.println( LocalDate.parse("2000-01-02", DateTimeFormatter.ISO_DATE) );
 
 	
 	}
