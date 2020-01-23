b912a96c5988c4f8a58857b8d318b1cf7e0fd220
tony <antonio.olvera@tecnoaplicada.com>
2020-01-22 23:18:53 -0600

    commit rama desarrollo

--- TrabajoComandosPrueba/src/com/test/ThreadMainFechas.java
+++ TrabajoComandosPrueba/src/com/test/ThreadMainFechas.java
@@ -13,7 +13,11 @@ public class ThreadMainFechas {
  		System.out.println( locadDate );
  		
  		System.out.println( locadDate );
- 		System.out.println( LocalDate.parse("2000-01-02", DateTimeFormatter.ISO_DATE) );
+		 System.out.println( LocalDate.parse("2000-01-02", DateTimeFormatter.ISO_DATE) );
+		 
+
+		 System.out.println( locadDate );
+		 
 
 	}
 } 
